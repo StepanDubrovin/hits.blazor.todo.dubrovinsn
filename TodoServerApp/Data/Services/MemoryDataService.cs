53546c3656bbceea615a74c83f2427c014d503c0
Stepa <stepan.09myromec@gmail.com>
2024-09-26 22:02:47 +0500

    async

---
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using TodoServerApp.Data.Interfaces;
+
+namespace TodoServerApp.Data.Services
+{
+    public class MemoryDataService : IDataService
+    {
+        private static IEnumerable<TaskItem> Tasks { get; } = [
+            new() {Id = 1, Title = "Задача 1", Description = "Описание", CreatedDate = DateTime.Now },
+            new() {Id = 1, Title = "Задача 2", Description = "Описание 2", CreatedDate = DateTime.Now },
+            new() {Id = 1, Title = "Задача 3", Description = "Описание 3", CreatedDate = DateTime.Now },
+        ];
+        public async Task<IEnumerable<TaskItem>>GetTaskItemsAsync()
+        {   
+            await Task.Delay(1000);
+            return await Task.FromResult(Tasks);
+        }
+    }
+}
